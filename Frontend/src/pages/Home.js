95d42811617f9452db2f38c11962a1160fc275c8
Anthony Terry <terryant64@gmail.com>
2025-04-12 20:16:18 -0400

    changed home page

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -17,22 +17,31 @@ function Home() {
         </div>
       </div>
 
-      {/* Content section for the text-boxes */}
-      <div className="text">
-        <div className="text-box-section">
-          <p>{"> PERSONAL"}</p>
+      {/* New container for text boxes and the paragraph box */}
+      <div className="content-container">
+        {/* Left side: Text boxes */}
+        <div className="text">
+          <div className="text-box-section">
+            <p>{"> PERSONAL"}</p>
+          </div>
+          <div className="text-box-section">
+            <p>{"> VIRTUAL"}</p>
+            <button className="styled-button" onClick={handleStart}>
+              {"> START <"}
+            </button>
+          </div>
+          <div className="text-box-section">
+            <p>{"> SHARED"}</p>
+          </div>
         </div>
-        <div className="text-box-section">
-          <p>{"> SHARED"}</p>
-        </div>
-        <div className="text-box-section">
-          <p>{"> VIRTUAL"}</p>
-          <button className="styled-button" onClick={handleStart}>
-            {"> START <"}
-          </button>
-        </div>
-        <div className="text-box-section">
-          <p>{"> DATA"}</p>
+
+        {/* Right side: Paragraph box */}
+        <div className="paragraph">
+          <div className="paragraph-box-section">
+            <p> {"What is the shared folder for? Who knows! But it's in the screenshots and so we're using it!"} </p>
+            <p> {"This is just going to be a really long-winded string literal that allows me to test such things as text wrapping, and scrolling."}</p>
+            <p> {"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus posuere libero at nulla dignissim porta. Etiam quam nibh, tempor et lectus id, viverra fringilla augue. Sed sed laoreet erat. Etiam tempor eget felis eget porta. Aenean purus arcu, venenatis et cursus."} </p>
+          </div>
         </div>
       </div>
     </div>
